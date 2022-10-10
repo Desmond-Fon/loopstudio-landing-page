7a6e72884c1e90ebb2d847b35dc9c96a50d75aae
Desmond-Fon <desmondfon@yahoo.com>
2022-10-10 12:58:04 +0100

    adjustments

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-const button = document.getElementById('fadeout');
-const hamburg = document.getElementById('hamburger')
 
-// toggle.addEventListener('click', () => {
-//     console.log('clickedd');
-// });
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu')
 
-button.addEventListener('click', function(){
-    // let hamburg = ``
-  
-    if (button){
-        hamburg.innerHTML += `<ul><li>hi</li></ul>`
-        // hamburg.textContent = `hiii`
-    // console.log(hamburg);
-    } else {
-        // humburg.textContent = ``
-    }
-    
-    
-})
+function toggleMenu () {
+  if (menu.classList.contains('active')){
+    menu.classList.remove('active');
+    toggle.querySelector('a').innerHTML = `<img src = 'images/icon-hamburger.svg'>`;
+  } else {
+    menu.classList.add('active');
+    toggle.querySelector('a').innerHTML = `<div class= 'mobile-nav'>
+    <div class='nav-header'>
+    <img class="logo" src="images/logo.svg" alt="" />
+    <img class="close" src="images/icon-close.svg" alt="" /> 
+    </div>
+    <div class='mobile-view'><ul class='navigation'>
+   
+            
+        
+    <li ><a href="#" class='children'> ABOUT </a></li>
+    <li ><a href="#" class='children'> CAREERS </a></li>
+    <li><a href="#" class='children'> EVENTS </a></li>
+    <li><a href="#" class='children'> PRODUCTS </a></li>
+    <li><a href="#" class='children'> SUPPORT </a></li>
+    </ul>
+    </div>
+    </div> `
+  }
+}
+toggle.addEventListener('click', toggleMenu, false);
+// const navItem =  document.querySelectorAll('.nav-item');
 
-console.log(hamburg);
+// hamburger.addEventListener('click', () => {
+//     console.log(navItem); // you can see your nav items in console everytime you click the button
+//     navItem.forEach(navItem => navItem.classList.toggle('show'))
+
+//     // navItem.classList.toggle('show');
+//   });
+
+// const navItems =  Array.from(document.querySelectorAll('.nav-item'));
+
+//  hamburger.addEventListener('click', () => {
+//    console.log(navItems); // you can see your nav items in console everytime you click the button
+//    navItems.forEach(navItem => navItem.classList.toggle('show'))
+//  });
